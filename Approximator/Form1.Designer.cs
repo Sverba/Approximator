3468107400ac7ba32be4412dbcf5e33a0b47bbcc
Artem Sverba <gloriks273@gmail.com>
2021-12-03 15:50:05 +0200

    Approximator

---
+++ Approximator/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Approximator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.buttonCalcX = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridTable = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numericUpDownBasis = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBasis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxX);
+            this.groupBox1.Controls.Add(this.buttonCalcX);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.buttonCalc);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dataGridTable);
+            this.groupBox1.Controls.Add(this.numericUpDownBasis);
+            this.groupBox1.Controls.Add(this.numericUpDownCount);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 571);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Введення даних";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(211, 483);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(202, 24);
+            this.textBoxX.TabIndex = 10;
+            // 
+            // buttonCalcX
+            // 
+            this.buttonCalcX.Location = new System.Drawing.Point(15, 524);
+            this.buttonCalcX.Name = "buttonCalcX";
+            this.buttonCalcX.Size = new System.Drawing.Size(398, 41);
+            this.buttonCalcX.TabIndex = 9;
+            this.buttonCalcX.Text = "Обчислити функцію";
+            this.buttonCalcX.UseVisualStyleBackColor = true;
+            this.buttonCalcX.Click += new System.EventHandler(this.buttonCalcX_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 489);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Значення аргумента X";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 434);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(12, 435);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(193, 38);
+            this.buttonCalc.TabIndex = 6;
+            this.buttonCalc.Text = "Розрахувати";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Вузли сітки";
+            // 
+            // dataGridTable
+            // 
+            this.dataGridTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridTable.Location = new System.Drawing.Point(10, 135);
+            this.dataGridTable.Name = "dataGridTable";
+            this.dataGridTable.RowTemplate.Height = 24;
+            this.dataGridTable.Size = new System.Drawing.Size(403, 283);
+            this.dataGridTable.TabIndex = 4;
+            this.dataGridTable.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridTable_DataError);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "X";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Y";
+            this.Column2.Name = "Column2";
+            // 
+            // numericUpDownBasis
+            // 
+            this.numericUpDownBasis.Location = new System.Drawing.Point(270, 62);
+            this.numericUpDownBasis.Name = "numericUpDownBasis";
+            this.numericUpDownBasis.Size = new System.Drawing.Size(143, 24);
+            this.numericUpDownBasis.TabIndex = 3;
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Location = new System.Drawing.Point(270, 26);
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(143, 24);
+            this.numericUpDownCount.TabIndex = 2;
+            this.numericUpDownCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCount.ValueChanged += new System.EventHandler(this.numericUpDownCount_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Степінь многочлена";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кількість вузлів";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBoxResult);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(474, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(488, 571);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Обчислення";
+            // 
+            // richTextBoxResult
+            // 
+            this.richTextBoxResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxResult.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.richTextBoxResult.Location = new System.Drawing.Point(20, 29);
+            this.richTextBoxResult.Name = "richTextBoxResult";
+            this.richTextBoxResult.ReadOnly = true;
+            this.richTextBoxResult.Size = new System.Drawing.Size(443, 536);
+            this.richTextBoxResult.TabIndex = 0;
+            this.richTextBoxResult.Text = "Дана програма дозваоляє виконати апроксимацію функції, заданої таблично, а також " +
+    "обчислити значення функції при деякому Х\n";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 591);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Апроксиматор";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBasis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Button buttonCalcX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridTable;
+        private System.Windows.Forms.NumericUpDown numericUpDownBasis;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBoxResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Approximator/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Approximator //оголошення блоку, в якому зберігається програма
+{
+    public partial class FormMain : System.Windows.Forms.Form //клас форми програми, в якому зберігається код
+    {
+        private double[] result; //загальна масиви розвя'зків системи рівнянь
+        private double[,] xyTable, matrix; //загальні масиви табличних даних та значень матриці
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void TableRemake() //зміна структури таблиці вхідних даних
+        {
+            //кількість рядків таблиці відповідає значенню "кількість вузлів"
+            dataGridTable.RowCount = Convert.ToInt32(numericUpDownCount.Value);
+            for (int i = 0; i < dataGridTable.RowCount; i++)
+            {
+                //встановлення типу даних в комірках
+                dataGridTable[0, i].ValueType = System.Type.GetType("System.Double");
+                dataGridTable[1, i].ValueType = System.Type.GetType("System.Double");
+                //В пустих комірках автоматично ставиться нуль
+                if (Convert.ToString(dataGridTable[0, i].Value) == "")
+                    dataGridTable[0, i].Value = 0;
+                if (Convert.ToString(dataGridTable[1, i].Value) == "")
+                    dataGridTable[1, i].Value = 0;
+            }
+        }
+
+        //метод для виклику розв'язання методом Гаусса
+        private double[] Gauss(double[,] matrix, int rowCount, int colCount)
+        {
+            int i; //індекс масиву
+            int[] mask = new int[colCount - 1]; //допоміжний масив індексів стовпців матриці
+            for (i = 0; i < colCount - 1; i++) mask[i] = i;
+            //ref використовується посилання на об'єкт для його безпосередньої зміни
+            //Виконання методу складається з двох етапів,
+            if (GaussDirectPass(ref matrix, ref mask, colCount, rowCount))
+            {
+                //тому другий етап почне працювати тільки після успішного виконання першого, який поверне true для цієї умови
+                double[] answer = GaussReversePass(ref matrix, mask, colCount, rowCount);
+                //другий етап повертає результат розв'язання
+                return answer;
+            }
+            //нульвое значення при невдалому виконані першого етапу
+            else return null;
+        }
+
+        //Прямий хід методу Гаусса
+        private bool GaussDirectPass(ref double[,] matrix, ref int[] mask, int colCount, int rowCount)
+        {
+            int i, j, k, maxId, tmpInt;
+            double maxVal, tempDouble;
+            for (i = 0; i < rowCount; i++) //цикл, який перебирає рядки матриці
+            {
+                maxId = i; //індекс найбільшого значення в рядку
+                maxVal = matrix[i, i]; //найбільше значення в рядку
+                //цикл перебиратиме значення рядку для пошуку найбільшого по модулю(головного)
+                for (j = i + 1; j < colCount - 1; j++)
+                    if (Math.Abs(maxVal) < Math.Abs(matrix[i, j]))
+                    {
+                        maxVal = matrix[i, j];
+                        maxId = j;
+                    }
+                if (maxVal == 0) return false; //рядок складається з нулів - розв'язку немає, завершення методу
+                //перестановка головного рядка "нагору" матриці
+                if (i != maxId)
+                {
+                    //перестановка значень
+                    for (j = 0; j < rowCount; j++)
+                    {
+                        tempDouble = matrix[j, i];
+                        matrix[j, i] = matrix[j, maxId];
+                        matrix[j, maxId] = tempDouble;
+                    }
+                    //перестановка вказівників на шуканий коефіцієнт відповідно до рядків
+                    tmpInt = mask[i];
+                    mask[i] = mask[maxId];
+                    mask[maxId] = tmpInt;
+                }
+                for (j = 0; j < colCount; j++) matrix[i, j] /= maxVal; //множники для неголовних рядків
+                for (j = i + 1; j < rowCount; j++)
+                {
+                    //почленне додавання помноженого головного рядка до неголовного
+                    double tempMn = matrix[j, i];
+                    for (k = 0; k < colCount; k++)
+                        matrix[j, k] -= matrix[i, k] * tempMn;
+                }
+            }
+            return true;
+        }
+
+        //Зворотній ход методу Гаусса
+        private double[] GaussReversePass(ref double[,] matrix, int[] mask, int colCount, int rowCount)
+        {
+            int i, j, k;
+            //аналогія прямому ходу, але вже з другого кінця матриці
+            for (i = rowCount - 1; i >= 0; i--)
+                for (j = i - 1; j >= 0; j--)
+                {
+                    double tempMn = matrix[j, i];
+                    for (k = 0; k < colCount; k++)
+                        matrix[j, k] -= matrix[i, k] * tempMn;
+                }
+            //створення і повернення массиву розв'язків
+            double[] answer = new double[rowCount];
+            for (i = 0; i < rowCount; i++) answer[mask[i]] = matrix[i, colCount - 1];
+            return answer;
+        }
+
+        //класс створення системи рівнянь
+        private double[,] MakeSystem(double[,] xyTable, int basis)
+        {
+            double[,] matrix = new double[basis, basis + 1];
+            for (int i = 0; i < basis; i++)
+            {
+                for (int j = 0; j < basis; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < basis; i++)
+            {
+                for (int j = 0; j < basis; j++)
+                {
+                    double sumA = 0, sumB = 0;
+                    for (int k = 0; k < dataGridTable.RowCount; k++)
+                    {
+                        sumA += Math.Pow(xyTable[0, k], i) * Math.Pow(xyTable[0, k], j);
+                        sumB += xyTable[1, k] * Math.Pow(xyTable[0, k], i);
+                    }
+                    matrix[i, j] = sumA;
+                    matrix[i, basis] = sumB;
+                }
+            }
+            return matrix;
+        }
+
+
+        //реакція на зміну значення "Кількість вузлів"
+        private void numericUpDownCount_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                TableRemake();
+                //максимально допустимий для пошуку степінь многочлена напряму залежить від кількості відомих даних
+                numericUpDownBasis.Maximum = numericUpDownCount.Value - 1;
+            }
+            catch
+            {
+                if (numericUpDownCount.Value < 1)
+                {
+                    MessageBox.Show("Кількість вузлів не може бути рівною 0");
+                    numericUpDownCount.Minimum = 1;
+                }
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            TableRemake();
+        }
+
+        private void dataGridTable_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            MessageBox.Show("Введене значення не є дійсним числом");
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridTable.RowCount; i++)
+            {
+                dataGridTable[0, i].Value = 0;
+                dataGridTable[1, i].Value = 0;
+            }
+            matrix = null;
+            result = null;
+            xyTable = null;
+
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            //виділення пам'яті під матрицю введених значень
+            xyTable = new double[2, dataGridTable.RowCount];
+            try
+            {
+                for (int i = 0; i < dataGridTable.RowCount; i++)
+                {
+                    xyTable[0, i] = Convert.ToDouble(dataGridTable[0, i].Value);
+                    xyTable[1, i] = Convert.ToDouble(dataGridTable[1, i].Value);
+                }
+            }
+            //якщо було введено нечислове значення
+            catch
+            {
+                MessageBox.Show("Одне із введених в таблицю значень не є дійсним числом");
+                richTextBoxResult.Select();
+                SendKeys.Send("^{END}");
+                return;
+            }
+            //запис у пам'ять значення "Степінь многочлена"
+            int basis = Convert.ToInt32(numericUpDownBasis.Value) + 1;
+            matrix = MakeSystem(xyTable, basis); //утворення матриці системи лінійних рівнянь
+            //виведення системи лінійних рівнянь
+            richTextBoxResult.Text += "-------------------------------------------------------------------------------\n";
+            richTextBoxResult.Text += "Створюємо систему рівнянь:\n";
+            for (int i = 0; i < basis; i++)
+            {
+                for (int j = 0; j < basis; j++)
+                {
+                    richTextBoxResult.Text += ((matrix[i, j] > 0) ? "+" : "") +
+                        Math.Round(matrix[i, j], 3).ToString() + "*x" + j.ToString() + " ";
+                }
+                richTextBoxResult.Text += " = " + matrix[i, basis] + "\n";
+            }
+            //підключаємо метод Гаусса, результат виконання якого записано в вище ініціалізований загальний масив
+            result = Gauss(matrix, basis, basis + 1);
+            //якщо метод не зміг виконати прямий хід
+            if (result == null)
+            {
+                richTextBoxResult.Text += "Неможливо знайти часткове рішення системи рівнянь\n";
+                richTextBoxResult.Select();
+                SendKeys.Send("^{END}");
+                return;
+            }
+            //виведення розв'язків
+            richTextBoxResult.Text += "Рішення системи рівнянь:\n";
+            for (int i = 0; i < basis; i++)
+            {
+                richTextBoxResult.Text += "C" + i.ToString() + " = " + Math.Round(result[i], 3).ToString() + "\n";
+            }
+            //виведення рівняння
+            richTextBoxResult.Text += "Апроксимація:\ny = ";
+            for (int i = 0; i < basis; i++)
+            {
+                if (Math.Round(result[i], 3) != 0)
+                    richTextBoxResult.Text += ((result[i] > 0) ? "+" : "") +
+                        Math.Round(result[i], 3).ToString() + ((i > 0) ? "*x^" + i.ToString() : "") + " ";
+            }
+            richTextBoxResult.Text += "\n";
+            richTextBoxResult.Select();
+            SendKeys.Send("^{END}");
+        }
+
+        private void buttonCalcX_Click(object sender, EventArgs e)
+        {
+            int basis = Convert.ToInt32(numericUpDownBasis.Value) + 1;
+            //якщо ще не знайдена апроксимація
+            if (result == null)
+            {
+                richTextBoxResult.Text += "Апроксимація функції ще не була проведена\n";
+                richTextBoxResult.Select();
+                SendKeys.Send("^{END}");
+                return;
+            }
+            double x;
+            try
+            {
+                //запис у пам'ять значення Х
+                x = Convert.ToDouble(textBoxX.Text);
+            }
+            //Якщо Х - не число
+            catch
+            {
+                MessageBox.Show("Введене значення Х не є дійсним числом");
+                richTextBoxResult.Select();
+                SendKeys.Send("^{END}");
+                return;
+            }
+            //розрахунок Y при такому Х
+            double y = 0;
+            for (int i = 0; i < basis; i++)
+            {
+                y += result[i] * Math.Pow(x, i);
+            }
+            //виведення результату
+            richTextBoxResult.Text += "f(" + x.ToString() + ") = " + Math.Round(y, 5).ToString() + " \n";
+            richTextBoxResult.Select();
+            SendKeys.Send("^{END}");
+
+        }
+
+    }
+}
